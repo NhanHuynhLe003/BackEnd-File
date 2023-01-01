2e9cdfeff2a176b3fe06166fcefb48242ed46716
NhanHuynh <nhan123huynh2k3@gmail.com>
2023-01-01 14:14:27 +0700

    MVC

--- src/app.js
+++ src/app.js
@@ -1,20 +1,4 @@
 
-/**
- * bai 11:
- * cach them file tinh:
- * b1: tao thu muc public chua file tinh
- * b2: ben trong app.js: app.use(express.static(path.join(__dirname, 'public'))); (*)
- * b3: public se chuyen thanh localhost:3000 de kiem tra=> localhost:3000/img/Nodejs_logo.png
- * 
- * cach cai scss
- * b1: cai dat: npm i node-sass --save-dev  
- * b2: trong resources tao file "scss", sau do trong file scss tao file "app.scss"
- *     dong thoi trong public tao "thu muc css" de lat convert
- * b3: vao file package.json trong "script" them << "watch":"node-sass --watch src/resources/scss/app.scss src/public/css/style.css" >>
- * b4: link css vao main: <link rel="stylesheet" href="/css/app.css">
- * b4.1: extend khi chi can luu file scss thi tu dong thay doi: "watch": "node-sass --watch src/resources/scss/ --output src/public/css/",
- * !important: luu y khi duong dan da thay doi sang public, thi luc nay link css: "/css/app.css"
- */
 
 const express = require('express');
 const { engine } = require("express-handlebars");
@@ -29,14 +13,14 @@ app.engine("hbs", engine({
 app.set("view engine", "hbs");
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.use(express.static(path.join(__dirname, 'public'))); //bai11
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(express.urlencoded({
-    extended: true //bai 14: cach ngan bao loi
-})); // xu ly duoi dang form html
-app.use(express.json()) //gui data tu js len sẻver
+    extended: true 
+})); 
+app.use(express.json()) 
 
-const port = 3000; //thinh thoang port co van de ta phai doi port
-const direct = '/bai11';
+const port = 3000; 
+const direct = '/bai30';
 app.get(direct, (req,res) => {
     res.render('home')
 });
@@ -44,8 +28,6 @@ app.get("/formSearch", (req,res) => {
     res.render('search')
 });
 app.post("/formSearch", (req,res) => {
-    console.log(req.body); //bai 14: req body ban dau = undefined, phai dung middleware mới hiển thị data.
-    // data hiển thị ra bên trong trình duyệt -> Network -> payload -> Form Data
     res.send("");
 })
 app.listen(port, () => {
